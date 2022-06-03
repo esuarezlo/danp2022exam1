35a60cec31a6bc0ee7fdc799c8392ce1fdbbd68d
Ernesto Suarez <ernesto@amicitech.com>
2022-06-03 11:58:16 -0500

    Initial commit

---
+++ app/src/main/java/org/idnp/danp2022exam1/MainActivityATipoD.java
@@ -0,0 +1,14 @@
+package org.idnp.danp2022exam1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityATipoD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_atipo_d);
+    }
+}
---
+++ app/src/main/java/org/idnp/danp2022exam1/MainActivityBTipoE.java
@@ -0,0 +1,14 @@
+package org.idnp.danp2022exam1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityBTipoE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_btipo_e);
+    }
+}
